810d3eadb5be583e838e6ce380c36723e40f193d
Evan Balzuweit <ebalzuweit@gmail.com>
2022-09-30 23:14:49 -0500

    update FlitterContext tests

--- tests/flitter.Tests/Data/FlitterContextTests.cs
+++ tests/flitter.Tests/Data/FlitterContextTests.cs
@@ -5,25 +5,56 @@ namespace flitter.Tests.Data;
 public class FlitterContextTests
 {
 	[Fact]
-	public async Task PerformDatabaseTests()
+	public async Task Ctor_CreatesSharedInMemoryDb()
 	{
-		const string filename = "flitter.db";
-
 		// setup in-memory database
 		await using FlitterContext ctx = new();
 		ctx.RegisterEntity<TestEntity>();
 		await ctx.CreateTableAsync<TestEntity>();
 		await ctx.InsertEntityAsync<TestEntity>(new TestEntity("!@#$%"));
-		// save to new file
-		File.Delete(filename);
+
+		// load another context
+		await using var ctx2 = new FlitterContext();
+		ctx2.RegisterEntity<TestEntity>();
+		var entities = await ctx2.GetEntitiesAsync<TestEntity>();
+
+		Assert.NotEmpty(entities);
+		Assert.All(entities, entity => Assert.True(entity.Id > 0));
+	}
+
+	[Fact]
+	public async Task README_Test()
+	{
+		const string filename = "flitter.db";
+
+		// setup in-memory database
+		await using FlitterContext ctx = new();
+		ctx.RegisterEntity<Person>();
+		// create table and entity
+		await ctx.CreateTableAsync<Person>();
+		await ctx.InsertEntityAsync<Person>(new Person("John", "Smith"));
+		// save to file
 		await ctx.Save(filename);
 		// load file
 		await using var ctx2 = new FlitterContext($"Data Source={filename}");
-		var events = await ctx.GetEntitiesAsync<TestEntity>();
+		ctx2.RegisterEntity<Person>();
+		var people = await ctx2.GetEntitiesAsync<Person>();
+	}
 
-		Assert.True(File.Exists(filename));
-		Assert.NotEmpty(events);
-		Assert.All(events, @event => Assert.True(@event.Id > 0));
+	internal class Person
+	{
+		[Key, Required, AutoIncrement] public int Id { get; }
+		[Required] public string Name { get; init; }
+		public string? Surname { get; init; }
+
+		public Person() : this(string.Empty, null) { }
+
+		public Person(string name, string? surname)
+		{
+			Id = -1;
+			Name = name;
+			Surname = surname;
+		}
 	}
 
 	internal class TestEntity
