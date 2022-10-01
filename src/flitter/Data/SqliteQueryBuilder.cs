92751a20e6ab16247e9e1575004719faf73067e0
Evan Balzuweit <ebalzuweit@gmail.com>
2022-09-30 21:06:36 -0500

    add CRUD scripts

--- src/flitter/Data/SqliteQueryBuilder.cs
+++ src/flitter/Data/SqliteQueryBuilder.cs
@@ -14,8 +14,6 @@ public class SqliteQueryBuilder<T>
 
 	public string CreateTableScript()
 	{
-		var type = typeof(T);
-
 		var sb = new StringBuilder();
 		sb.AppendFormat("CREATE TABLE IF NOT EXISTS {0} (", _mapping.TableName);
 
@@ -38,5 +36,103 @@ public class SqliteQueryBuilder<T>
 		sb.Append(")");
 
 		return sb.ToString();
+	}
+
+	public string InsertEntityScript()
+	{
+		var sb = new StringBuilder();
+		sb.AppendFormat("INSERT INTO {0} (", _mapping.TableName);
+
+		// TODO: only loop once
+		bool firstColumn = true;
+		foreach (var column in _mapping.Columns)
+		{
+			if (column.AutoIncrement)
+				continue;
+
+			if (firstColumn)
+				firstColumn = false;
+			else
+				sb.Append(", ");
+
+			sb.Append(column.Name);
+		}
+		sb.Append(") VALUES (");
+
+		firstColumn = true;
+		foreach (var column in _mapping.Columns)
+		{
+			if (column.AutoIncrement)
+				continue;
+
+			if (firstColumn)
+				firstColumn = false;
+			else
+				sb.Append(", ");
+
+			sb.AppendFormat(":{0}", column.Name);
+		}
+		sb.Append(")");
+
+		return sb.ToString();
+	}
+
+	public string SelectEntitiesScript()
+	{
+		var sb = new StringBuilder();
+		sb.AppendFormat("SELECT * FROM {0}", _mapping.TableName);
+		return sb.ToString();
+	}
+
+	public string SelectEntityByPrimaryKeyScript()
+	{
+		ValidateMappingHasPrimaryKey();
+
+		var sb = new StringBuilder();
+		sb.AppendFormat("SELECT * FROM {0}", _mapping.TableName);
+		sb.AppendFormat(" WHERE {0} = :{0}", _mapping.PrimaryKey!.Name);
+		return sb.ToString();
+	}
+
+	public string UpdateEntityByPrimaryKeyScript()
+	{
+		ValidateMappingHasPrimaryKey();
+
+		var sb = new StringBuilder();
+		sb.AppendFormat("UPDATE {0} SET ", _mapping.TableName);
+
+		var firstColumn = true;
+		foreach (var column in _mapping.Columns)
+		{
+			if (column.IsPrimaryKey)
+				continue;
+			if (column.AutoIncrement)
+				continue;
+
+			if (firstColumn)
+				firstColumn = false;
+			else
+				sb.Append(", ");
+
+			sb.AppendFormat("{0} = :{0}", column.Name);
+		}
+
+		sb.AppendFormat(" WHERE {0} = :{0}", _mapping.PrimaryKey!.Name);
+		return sb.ToString();
+	}
+
+	public string DeleteEntityByPrimaryKeyScript()
+	{
+		ValidateMappingHasPrimaryKey();
+
+		var sb = new StringBuilder();
+		sb.AppendFormat("DELETE FROM {0} WHERE {1} = :{1}", _mapping.TableName, _mapping.PrimaryKey!.Name);
+		return sb.ToString();
+	}
+
+	private void ValidateMappingHasPrimaryKey()
+	{
+		if (_mapping.PrimaryKey is null)
+			throw new InvalidOperationException("Mapping has no primary key.");
 	}
 }
--- tests/flitter.Tests/Data/SqliteQueryBuilderTests.cs
+++ tests/flitter.Tests/Data/SqliteQueryBuilderTests.cs
@@ -1,7 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using Dapper;
 using flitter.Data;
-using flitter.Data.Commands;
 using Microsoft.Data.Sqlite;
 
 namespace flitter.Tests.Data;
@@ -17,27 +16,99 @@ public class SqliteQueryBuilderTests
 		var createTableScript = queryBuilder.CreateTableScript();
 		await ctx.ExecuteAsync(new TestCommand(createTableScript));
 
-		Assert.Equal("CREATE TABLE IF NOT EXISTS TestEntity (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)", createTableScript);
+		Assert.Equal("CREATE TABLE IF NOT EXISTS TestEntity (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Message TEXT)", createTableScript);
+	}
+
+	[Fact]
+	public async Task InsertEntityScript_ProducesValidSQL()
+	{
+		var ctx = new FlitterContext();
+		var queryBuilder = new SqliteQueryBuilder<TestEntity>();
+		await ctx.ExecuteAsync(new TestCommand(queryBuilder.CreateTableScript()));
+
+		var insertEntityScript = queryBuilder.InsertEntityScript();
+		await ctx.ExecuteAsync(new TestCommand(insertEntityScript, new TestEntity()));
+
+		Assert.Equal("INSERT INTO TestEntity (Message) VALUES (:Message)", insertEntityScript);
+	}
+
+	[Fact]
+	public async Task SelectEntitiesScript_ProducesValidSQL()
+	{
+		var ctx = new FlitterContext();
+		var queryBuilder = new SqliteQueryBuilder<TestEntity>();
+		await ctx.ExecuteAsync(new TestCommand(queryBuilder.CreateTableScript()));
+
+		var selectEntitiesScript = queryBuilder.SelectEntitiesScript();
+		await ctx.ExecuteAsync(new TestCommand(selectEntitiesScript));
+
+		Assert.Equal("SELECT * FROM TestEntity", selectEntitiesScript);
+	}
+
+	[Fact]
+	public async Task SelectEntityByPrimaryKeyScript_ProducesValidSQL()
+	{
+		var ctx = new FlitterContext();
+		var queryBuilder = new SqliteQueryBuilder<TestEntity>();
+		await ctx.ExecuteAsync(new TestCommand(queryBuilder.CreateTableScript()));
+		// await ctx.ExecuteAsync(new TestCommand(queryBuilder.InsertEntityScript(), new TestEntity()));
+
+		var selectEntityByIdScript = queryBuilder.SelectEntityByPrimaryKeyScript();
+		await ctx.ExecuteAsync(new TestCommand(selectEntityByIdScript, new TestEntity()));
+
+		Assert.Equal("SELECT * FROM TestEntity WHERE Id = :Id", selectEntityByIdScript);
+	}
+
+	[Fact]
+	public async Task UpdateEntityByPrimaryKeyScript_ProducesValidSQL()
+	{
+		var ctx = new FlitterContext();
+		var queryBuilder = new SqliteQueryBuilder<TestEntity>();
+		await ctx.ExecuteAsync(new TestCommand(queryBuilder.CreateTableScript()));
+		await ctx.ExecuteAsync(new TestCommand(queryBuilder.InsertEntityScript(), new TestEntity()));
+
+		var updateEntityByIdScript = queryBuilder.UpdateEntityByPrimaryKeyScript();
+		await ctx.ExecuteAsync(new TestCommand(updateEntityByIdScript, new TestEntity()));
+
+		Assert.Equal("UPDATE TestEntity SET Message = :Message WHERE Id = :Id", updateEntityByIdScript);
+	}
+
+	[Fact]
+	public async Task DeleteEntityByPrimaryKeyScript_ProducesValidSQL()
+	{
+		var ctx = new FlitterContext();
+		var queryBuilder = new SqliteQueryBuilder<TestEntity>();
+		await ctx.ExecuteAsync(new TestCommand(queryBuilder.CreateTableScript()));
+		await ctx.ExecuteAsync(new TestCommand(queryBuilder.InsertEntityScript(), new TestEntity()));
+
+		var deleteEntityByIdScript = queryBuilder.DeleteEntityByPrimaryKeyScript();
+		await ctx.ExecuteAsync(new TestCommand(deleteEntityByIdScript, new TestEntity()));
+
+		Assert.Equal("DELETE FROM TestEntity WHERE Id = :Id", deleteEntityByIdScript);
 	}
 
 	internal class TestEntity
 	{
 		[Key, Required, AutoIncrement]
 		public int Id { get; init; }
+
+		public string Message { get; init; } = string.Empty;
 	}
 
 	internal class TestCommand : ICommand
 	{
 		public string Text { get; init; }
+		public object? Parameters { get; init; }
 
-		public TestCommand(string commandText)
+		public TestCommand(string commandText, object? parameters = null)
 		{
 			Text = commandText;
+			Parameters = parameters;
 		}
 
 		public async Task ExecuteAsync(SqliteConnection connection, CancellationToken cancellationToken = default)
 		{
-			await connection.ExecuteAsync(Text);
+			await connection.ExecuteAsync(Text, param: Parameters);
 		}
 	}
 }
